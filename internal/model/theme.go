381844fd6a8ebc91d30651bd7cd84ad7e9103d4a
Alexander Brandt <github@a13x.de>
2026-07-12 00:07:13 +0300

    refactor: delete dead code (pkg/theme/, model.ThemeDefinition)

--- internal/model/theme.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type ThemeDefinition struct {
-	ID     string            `json:"id"`
-	Name   string            `json:"name"`
-	Type   string            `json:"type"`
-	Colors map[string]string `json:"colors"`
-}
